1ffa4193026eb05df5cf9299eb50ef2e716b6c88
Fletch S <stampsfj04@gmail.com>
2021-02-04 13:02:18 -0500

    added methods to access githup and spaceX API'

---
+++ index.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch')
+
+// fetch('https://espn.com') // url, endpoint
+// .then(response => {
+//     return response.text()
+// } )
+
+// fetch('https://api.github.com/users/fletch8')
+// .then(response => {
+//     return response.json();
+// })
+// .then(data => {
+//     console.log(data)
+// })
+
+fetch('https://api.spacexdata.com/v3/capsules')
+.then(response => {
+    return response.json();
+})
+.then(dataArray => {
+    // console.log(dataArray)
+    let c102 = dataArray[1];
+    let capsuleSerial = c102['capsule_serial']
+    let capstat = c102.status
+    console.log(c102)
+    console.log(capsuleSerial)
+})
